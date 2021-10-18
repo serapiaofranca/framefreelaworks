75be67545b5b0ea686b987f645184e2d37544a04
Rogerio A Serapiao <rserapa@gmail.com>
2021-10-18 20:41:21 -0300

    developer view owned proposal in project

--- spec/system/developer_send_proposal_spec.rb
+++ spec/system/developer_send_proposal_spec.rb
@@ -72,4 +72,38 @@ describe 'Developer send proposal' do
 		expect(page).to have_content('10/02/2022')
 		expect(page).to have_link('Voltar')
 	end
+
+	it 'developer view owned proposal in project' do 		
+		julia = Manager.create!(email: 'julia@manager.com', password: '123456')
+        library_cloud = Project.create!(title: 'Library cloud', description: 'Locação virtual de livros', 
+                    requirements: 'Ruby, Raisl,Html, CSS, Javascript', 
+                    hourly_rate: 100, expiration_date: '10/05/2022', 
+                    start_date: '15/05/2022',end_date: '10/07/2022',manager: julia)
+
+        pablo = Developer.create!(email: 'pablo@developer.com', password:'123456')
+		Profile.create!(full_name: 'Pablo Estevez', social_name: 'paulo', 
+			birth_date: '10/05/1992', education: 'Engenharia de Software', 
+			skills: 'Ruby, Php, Javascript, CSS, Html, SqlServer', 
+			employment_history: 'Nasa Network, 3 anos', developer: pablo )		
+
+		Proposal.create!(motivation: 'Experiência em projetos Rails', hourly_rate: 80,
+						weekly_available_hours: 20, expected_completion: '01/07/2022', 
+						developer: pablo, project: library_cloud)
+
+		visit root_path
+		click_on 'Entrar como Desenvolvedor'
+		fill_in 'E-mail', with: pablo.email
+		fill_in 'Senha', with: pablo.password
+		click_on 'Entrar'		
+		click_on 'Library cloud'		
+
+		expect(current_path).to eq project_path(library_cloud)		
+		expect(page).to have_content 'pablo@developer.com'
+		expect(page).to have_content 'R$ 80,00'
+		expect(page).to have_content /20/
+		expect(page).to have_content '01/07/2022'
+		expect(page).to have_content 'Status: pendente'		
+		expect(page).not_to have_link 'Enviar proposta'
+	end
+	
 end
